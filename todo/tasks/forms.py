0136778168e9ca743d85c865883650ea8792ef59
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-06-10 15:20:35 +0300

    adding forms

--- todo/tasks/forms.py
+++ todo/tasks/forms.py
@@ -6,3 +6,6 @@ class TodoForm(forms.ModelForm):
     class Meta:
         model = TodoItem
         fields = "__all__"   # ['task', 'description', 'due_date', 'completed']
+        widgets = {
+            'due_date': forms.DateInput(attrs={'type': 'date'}),
+        }
