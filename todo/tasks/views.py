23f32f309d597ba56a084f6c9b2cb41cacd2bfee
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-06-11 08:05:20 +0300

    Adding the index and adding task functions

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import TodoItem
+from .forms import TodoForm
 
 # Create your views here.
+
+
+def index(request):
+    todos = TodoItem.objects.all()
+    return render(request, 'tasks/index.html', {'todos': todos})
+
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = TodoForm()
+    return render(request, 'tasks/add_task.html', {'form': form})
